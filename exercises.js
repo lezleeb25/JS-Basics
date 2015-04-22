860c8d00c5918468c513759c341ff23807679e31
Lezlee Beckstead <lezleeb@gmail.com>
2015-04-22 17:17:52 -0600

    update practice.js

---
+++ exercises.js
@@ -0,0 +1,43 @@
+var a = 5;
+var b = -1;
+
+var c = function min(a, b) {
+	Math.min(a, b);
+}
+console.log(c);
+
+console.log(min(0, 10));
+console.log(min(0, -10));
+
+
+
+
+
+
+
+function findSolution(target) {
+  function find(start, history) {
+    if (start == target)
+      return history;
+    else if (start > target)
+      return null;
+    else
+      return find(start + 5, "(" + history + " + 5)") ||
+             find(start * 3, "(" + history + " * 3)");
+  }
+  return find(1, "1");
+}
+
+console.log(findSolution(24));
+// → (((1 * 3) + 5) * 3)
+
+
+funtion isEven(num) {
+	if(num - 2 === 0) {
+		return true;
+	}
+	else {
+
+		return false;
+	}
+}
--- practice.js
+++ practice.js
@@ -1,13 +1,23 @@
 //Once you complete a problem, open up Chrome and check the answer in the console.
 
+//NOTES:
+//truinary statement
+// var x = 0 < 5 ? "true" : "false";
+// alert(x);
 
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only argument.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
 
   //Code Here
-
-
+var isTyler = function (name) {
+	if(name === "Tyler") {
+		return true;
+	} else {
+		return false;
+	}
+}
+console.log(isTyler(name));
 //Next problem
 
 
@@ -16,7 +26,12 @@ var name = 'Tyler';
 
 
   //Code Here
-
+var getName = function () {
+	// return prompt("What is your name?");
+	var name = prompt("What is your name?");
+	return name;
+}
+getName();
 
 //Next Problem
 
@@ -26,8 +41,11 @@ var name = 'Tyler';
 //then alerts "Welcome, " plus whatever the users name is.
 
   //Code Here
-
-
+var welcome = function(){
+	var name = getName();
+	alert("Welcome " + name);
+}
+welcome();
 //Next problem
 
 
@@ -36,6 +54,10 @@ var name = 'Tyler';
 //What is the difference between arguments and parameters?
 
   //Answer Here
+//The parameters are the aliases for the values that will be passed to the function. The arguments are the actual values.
+var foo = function( a, b, c ) {}; // a, b, and c are the parameters
+
+foo( 1, 2, 3 ); // 1, 2, and 3 are the arguments
 
 
 //Next problem
@@ -46,8 +68,17 @@ var name = 'Tyler';
 
 
   //Answer Here
-
-
+false
+0 (zero)
+"" (empty string)
+null
+undefined
+NaN (a special Number value meaning Not-a-Number!)
+// double equals
+var c = (false == 0); // true
+var d = (false == ""); // true
+var e = (0 == ""); // true
+console.log(null == undefined);
 
 //Next Problem
 
@@ -56,13 +87,16 @@ var name = 'Tyler';
 //Create a function called myName that returns your name
 
   //Code Here
-
+var myName = function(name){
+	return name;
+}
+myName("Lezlee");
 //Now save the function definition of myName into a new variable called newMyName
 
   //Code Here
-
+var newMyName = myName("Lezlee");
 //Now alert the result of invoking newMyName
-
+alert(newMyName);
 
 
 //Next problem
@@ -72,9 +106,20 @@ var name = 'Tyler';
 //Create a function called outerFn which returns an anonymous function which returns your name.
 
   //Code Here
+var outerFn = function(){
+	var name = "Lezlee";
+	return (function() {
+
+	})();
+}
 
 //Now save the result of invoking outerFn into a variable called innerFn.
 
   //Code Here
+var innerFn = (function (name){alert(name)})
 
 //Now invoke innerFn.
+('Lezlee');
+
+
+
